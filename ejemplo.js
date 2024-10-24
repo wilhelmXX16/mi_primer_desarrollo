a9f824db63147a29d9eefc6f4de0eced9fd5c713
wilhelmXX16 <wjc1619t@gmail.com>
2024-10-24 12:15:44 -0500

    mi primer commit

---
+++ ejemplo.js
@@ -0,0 +1,18 @@
+// nunca declarar una variable con var
+
+let a = {
+    nombre: "pedro",
+    edad: 13,
+    genero: "m",
+}; 
+let b = [1,2,3,4,5];
+let c = 45;
+let d = "hola";
+let ed = 'hola';
+
+console.log(ed);//! es para imprima el resultado por consola
+console.log('el nombre que esta guardado en a es :' + a.nombre)
+console.log(b[0])
+
+let areaCuadrado = 0
+
---
+++ ejemplo1.js
@@ -0,0 +1,22 @@
+// variables 
+// var 
+
+//var a = 1
+var a = 2
+console.log(a)
+// let 
+let b = 1
+// const
+const pi = 3.1416
+const meses = 12
+
+// var nombre = "juan"
+// var edad = 23
+
+// var nombre1 = "pedro"
+// nombre = nombre1
+
+// console.log(nombre)
+
+// area de un cuadrado, rectangulo y triangulo
+
